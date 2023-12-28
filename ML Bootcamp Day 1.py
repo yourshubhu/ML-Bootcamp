064015b4e39f8c7d44533921c0c6056b92a028aa
Shubham Sali <128118430+yourshubhu@users.noreply.github.com>
2023-12-28 23:32:19 +0530

    Add files via upload

---
+++ ML Bootcamp Day 1.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Basic day 1
+# 
+# 
+# a=10
+# b=20
+# c=a+b
+# print(c)
+#                 
+
+# In[40]:
+
+
+print("Welcome to python programming class")
+
+
+# In[38]:
+
+
+def test():
+    a=30
+    b=40
+    c=a+b
+    print(c)
+    
+
+
+# In[5]:
+
+
+test()
+
+
+# In[6]:
+
+
+test()
+
+
+# In[8]:
+
+
+test()
+
+
+# In[10]:
+
+
+a=10
+
+
+# In[11]:
+
+
+print(a)
+
+
+# In[12]:
+
+
+b=20
+
+
+# In[13]:
+
+
+print(b)
+
+
+# In[14]:
+
+
+print(a)
+print(b)
+
+
+# In[18]:
+
+
+print("this is my first number",a)
+
+
+# In[19]:
+
+
+print("this is my second number",b)
+
+
+# In[20]:
+
+
+print(a)
+print(b)
+
+
+# ## Sep
+
+# In[21]:
+
+
+print(a,b,sep="",end="")
+
+
+# In[22]:
+
+
+print(a,b,c,sep="=")
+
+
+# In[24]:
+
+
+print(a,b,c,sep=",")
+
+
+# ## Input Function
+
+# ## It is responsible for taking input from user
+
+# In[31]:
+
+
+input()
+
+
+# In[32]:
+
+
+input("First number:")
+
+
+# In[33]:
+
+
+a=input("Please write any number:")
+
+
+# In[34]:
+
+
+print(a)
+
+
+# In[35]:
+
+
+type(a)
+
+
+# ## Data types & Memory management 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+

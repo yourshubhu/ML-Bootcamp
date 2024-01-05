6bee517c422a8a25d64475b80224701510c12467
Shubham Sali <128118430+yourshubhu@users.noreply.github.com>
2024-01-05 22:23:24 +0530

    Add files via upload

---
+++ ML Bootcamp day 3.py
@@ -0,0 +1,219 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Indecing 
+
+# In[20]:
+
+
+a="shubham sali"
+
+
+# In[2]:
+
+
+a[4]
+
+
+# In[3]:
+
+
+a[1]
+
+
+# In[7]:
+
+
+a[0]
+
+
+# In[9]:
+
+
+a[0]
+
+
+# In[10]:
+
+
+a[0]
+
+
+# In[12]:
+
+
+a[0]
+
+
+# In[13]:
+
+
+a[-2]
+
+
+# In[19]:
+
+
+len(a)-1
+
+
+# In[21]:
+
+
+len(a)
+
+
+# In[22]:
+
+
+len(a)-1
+
+
+# ## Sring concatenatio 
+
+# In[23]:
+
+
+a="Shubham"
+
+
+# In[24]:
+
+
+b="sali"
+
+
+# In[25]:
+
+
+a+b
+
+
+# In[28]:
+
+
+a+" "+b
+
+
+# In[29]:
+
+
+a="Shubham"
+b="100"
+
+
+# In[32]:
+
+
+print(a+" "+b)
+
+
+# ### The slicer operator
+
+# In[37]:
+
+
+a="AASHISH"
+
+
+# In[38]:
+
+
+type(a)
+
+
+# In[34]:
+
+
+len(a)-1
+
+
+# In[35]:
+
+
+a[2:5]
+
+
+# In[39]:
+
+
+a[1:6]
+
+
+# In[40]:
+
+
+a="python"
+
+
+# In[41]:
+
+
+a[2:2]
+
+
+# In[42]:
+
+
+a[6:10]
+
+
+# In[43]:
+
+
+word="Shubhamsali"
+
+
+# In[46]:
+
+
+word[0:11]
+
+
+# In[44]:
+
+
+len(word)
+
+
+# In[45]:
+
+
+word[0:11:2]
+
+
+# In[47]:
+
+
+word="Shubham"
+
+
+# In[48]:
+
+
+word[-1:-4]
+
+
+# In[49]:
+
+
+word="I am here for learing data science"
+
+
+# In[50]:
+
+
+len(word)
+
+
+# In[52]:
+
+
+word[34:15:1]
+
+
+# In[ ]:
+
+
+
+
